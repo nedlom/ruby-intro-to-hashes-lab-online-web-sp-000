16377981f8f886fbbb892a7b97f1a6ae983797eb
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-15 20:33:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,13 +2,13 @@ def new_hash
   {}
 end
 
-def actor(name)
-  actor = {:name => name}
+def actor
+  actor = {:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
 	monopoly = {}
-
+	monopoly[:railroads] = {}
 end
 
 def monopoly_with_second_tier
