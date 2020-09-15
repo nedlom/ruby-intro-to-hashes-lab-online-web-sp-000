8939aa1d13b36568c8f8a3549d3c24c54b493766
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-15 20:47:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -47,10 +47,22 @@ def monopoly_with_fourth_tier
 	  :railroads => 
 	  {
 	    :pieces => 4,
-	    :names => {:reading_railroad => {'mortgage_value' => '$100'},
-	      :pennsylvania_railroad => {'mortgage_value' => '$100'},
-	      :b_and_o_railroad => {'mortgage_value' => '$100'},
-	      :shortline => {}
+	    :names => {:reading_railroad => {
+	      'mortgage_value' => '$100'
+	      
+	    },
+	      :pennsylvania_railroad => {
+	        'mortgage_value' => '$200'
+	        
+	      },
+	      :b_and_o_railroad => {
+	        'mortgage_value' => '$400'
+	        
+	      },
+	      :shortline => {
+	        'mortgage_value' => '$800'
+	        
+	      }
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
